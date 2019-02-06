9afc046099e18adfb74c753ffbc0e0bd9f3179d5
Johnny Tardin <johnny.tardin@m4u.com.br>
2019-02-06 13:55:31 -0200

    remove outliers.

---
+++ data_science/remove_outliers.py
@@ -0,0 +1,16 @@
+from random import randint
+
+import numpy as np
+
+values = [randint(0, 90000) for p in range(0, 120)]
+
+values_series = np.array(values)
+
+mean = np.mean(values_series, axis=0)
+standard_d = np.std(values_series, axis=0)
+
+new_data = [x for x in values if (x > mean - 2 * standard_d)]
+new_data = [x for x in new_data if (x < mean + 2 * standard_d)]
+
+print(sorted(new_data))
+
