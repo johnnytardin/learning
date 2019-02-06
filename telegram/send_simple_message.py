5b66b7af5202557d79b1dae047133ecca91b3922
Johnny Tardin <johnny.tardin@m4u.com.br>
2019-02-06 17:33:20 -0200

    telegram send message.

---
+++ telegram/send_simple_message.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import time
+
+import telegram
+
+# https://core.telegram.org/bots#generating-an-authorization-token
+my_token = ''
+
+def send(msg, chat_id, token=my_token):
+	"""
+    send a specific message to user.
+
+    chat_id: id to send a sendMessage.
+    Example group: https://web.telegram.org/#/im?p=g111111111, chat_id is:
+    '-111111111'
+	"""
+	bot = telegram.Bot(token=token)
+	bot.sendMessage(chat_id=chat_id, text=msg)
+
+if __name__ == '__main__':
+    chat_id = ''
+    while True:
+        message = f'Current date and UTC time: {datetime.utcnow().isoformat()}'
+        send(message, chat_id)
+        time.sleep(30)
