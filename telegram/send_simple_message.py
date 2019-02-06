b9f4640b0ff9bb82c2d00ea26f10a158a329fda6
Johnny Tardin <johnny.tardin@m4u.com.br>
2019-02-06 19:05:03 -0200

    refactoring indentation.

--- telegram/send_simple_message.py
+++ telegram/send_simple_message.py
@@ -10,14 +10,15 @@ bot = telegram.Bot(token=token)
 
 
 def send(msg, chat_id):
-	"""
+    """
     send a specific message to user.
 
     chat_id: id to send a sendMessage.
     Example group: https://web.telegram.org/#/im?p=g111111111, chat_id is:
     '-111111111'
-	"""
-	bot.sendMessage(chat_id=chat_id, text=msg)
+    """
+    bot.sendMessage(chat_id=chat_id, text=msg)
+
 
 if __name__ == '__main__':
     chat_id = ''
